020d054ce44c70abc9fe54fe7725dab3e2666299
Jmb71 <Jmb71@mail.ru>
2023-01-22 21:38:17 +0300

    add PDF akb

--- app/src/main/java/com/example/emergency_maps/AkbActivity.java
+++ app/src/main/java/com/example/emergency_maps/AkbActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class AkbActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_akb);
+
+
+
+      PDFView pdfView=findViewById(R.id.akbView);
+      pdfView.fromAsset("akb1.pdf").load();
     }
 }
